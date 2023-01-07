bb0a10d3fa4d3c11d64f052fce4ee446a2449650
starlyn1232 <41921942+starlyn1232@users.noreply.github.com>
2023-01-07 01:10:00 -0400

    Create hidden_password.c
    Hi, some years ago I tried to reach it (hidden password) in C++ but I couldn't, but now studying C I do, so, that's the short story. :)

---
+++ hidden_password.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+#define CORRECT "hola"
+
+void ReadPassword(char *data,const char SHOWING_CHAR){
+	//Declare temporal variables
+	char c = 'a';
+	int sz = 0;
+	
+	do{
+		//Wait for keyboard
+		c = getch();
+		
+		//Insert only non-return values
+		if (c != '\r' && c != '\b'){
+      //Show allowed char for hidden characters
+			printf("%c",SHOWING_CHAR);
+			*(data+sz) = c;
+			sz++;
+		}
+		
+		if (c == '\b' && sz > 0){
+			//"Print" the backspace + space, then remove back that last space with backspace
+			printf("\b \b");
+			sz--;
+			continue;
+		}
+		//Check non-return values
+	}while(c != '\r');
+	
+	//Set null value
+	*(data+(sz)) = '\0';
+}
+
+int main(){
+	
+	//Function pointer to use code block memory location
+	void (*ptr)(char *,char);
+	ptr = ReadPassword;
+	
+	//Char pointer for our word (Memory allocation used)
+	char *word = ((char *)malloc(sizeof(char *)));	
+	
+	//Read input (Hiddem)
+	printf("Input the password:");
+	//Call password reader function (Using delegate simulation)
+	ptr(word,'*');
+	
+	//Check password
+	if (strcmp(word,CORRECT) != 0){
+		//Error msg with alert escape
+		fprintf(stderr,"\n\aIncorrect password!",word);
+		exit(EXIT_FAILURE);
+	}
+	
+	//Free malloc after granted
+	free(word);
+	
+	//Correct password message
+	printf("\nAccess granted!");
+	exit(EXIT_SUCCESS);
+	
+	return 0;
+}
